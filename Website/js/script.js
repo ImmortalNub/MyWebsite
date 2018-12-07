23b7d7bd97f4200ac0f70cda68332051dce989d4
ImmortalNub <bjanis@live.com>
2018-12-06 18:15:41 -0600

    Dropdown menu update
    - Made dropdown menus within dropdown menus.
    
    - Cleaned up css code, removing a bunch of commented code.
    
    - Not sure how to implement object-oriented in javascript yet, so I put comments where I would have created the objects.
    
    - Sorted out javascript methods.

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,8 +1,29 @@
+// General
 function initialization() {
+	populateDropdowns();
+}
+
+function populateDropdowns() {
 	basicTutorials();
 	buckyTutorials();
 }
 
+function dropdownList(id, titles) {
+	// Create list items with a URL and a title.
+	var link = document.getElementById(id);
+	for (var url in titles) {
+		// Create the list element
+		var newLink = document.createElement("li");
+
+		// Populate list element with an anchor with the url and a label.
+		newLink.innerHTML = "<a href=" + url + " target='_blank' rel='noopener noreferrer'>"
+								+ titles[url] +
+							"</a>";
+		link.appendChild(newLink);
+	}
+}
+
+// W3Schools
 function basicTutorials() {
 	// Set element ID.
 	var id = "w3schools";
@@ -19,9 +40,27 @@ function basicTutorials() {
 	dropdownList(id, titles);
 }
 
+// TheNewBoston
 function buckyTutorials() {
+	buckyHTML();
+	buckyAngularJS();
+}
+
+function buckyAngularJS() {
 	// Set element ID.
-	var id = "bucky-tutorials";
+	var id = "bucky-angular-js";
+	// Create an array-like object for titles.
+	var titles = {
+			"https://www.youtube.com/watch?v=hXfigUyeHaY&list=PL6gx4Cwl9DGBYxWxJtLi8c6PGjNKGYGZZ&ab_channel=thenewboston": "1: Getting Started",
+	};
+
+	// Call dropdownList with W3Schools titles.
+	dropdownList(id, titles);
+}
+
+function buckyHTML() {
+	// Set element ID.
+	var id = "bucky-html";
 	// Create an array-like object for titles.
 	var titles = {
 			"https://www.youtube.com/watch?v=yNtIO4X6cag&index=12&list=PL081AC329706B2953&ab_channel=thenewboston":          "12: Making Everything Pretty",
@@ -32,19 +71,4 @@ function buckyTutorials() {
 
 	// Call dropdownList with W3Schools titles.
 	dropdownList(id, titles);
-}
-
-function dropdownList(id, titles) {
-	// Create list items with a URL and a title.
-	var link = document.getElementById(id);
-	for (var url in titles) {
-		// Create the list element
-		var newLink = document.createElement("li");
-
-		// Populate list element with an anchor with the url and a label.
-		newLink.innerHTML = "<a href=" + url + " target='_blank' rel='noopener noreferrer'>"
-								+ titles[url] +
-							"</a>";
-		link.appendChild(newLink);
-	}
 }
